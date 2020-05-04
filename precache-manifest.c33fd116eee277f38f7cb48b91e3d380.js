6989a91725e95fdd6cf55f591c29423a91873204
Radoslaw Stan <radoslaw.stan96@gmail.com>
2020-05-04 06:34:38 +0200

    Updates

---
+++ precache-manifest.c33fd116eee277f38f7cb48b91e3d380.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62fcc0db1206bdfb7cf2fb1d30c59f23",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "a0674e88ad2f619f0725",
+    "url": "/covid-tracker/static/css/main.6af3c402.chunk.css"
+  },
+  {
+    "revision": "91a41641c7b4c1ab9fc7",
+    "url": "/covid-tracker/static/js/2.75887e03.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.75887e03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0674e88ad2f619f0725",
+    "url": "/covid-tracker/static/js/main.f5a2a7b0.chunk.js"
+  },
+  {
+    "revision": "3dea3109efc02db259fe",
+    "url": "/covid-tracker/static/js/runtime-main.667d5dfa.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
